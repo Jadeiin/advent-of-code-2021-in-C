54289b251221bf1ed957332a47d3c2392e55b011
Jadeiin <92222981+Jadeiin@users.noreply.github.com>
2021-12-02 23:30:41 +0800

    Update 1-2.c

--- 1-2.c
+++ 1-2.c
@@ -4,8 +4,7 @@ int main(void)
 	int i,n=0,num[3]={0},count=0;
 	while(scanf("%d",&i)!=EOF)
 	{
-		if(num[n%3]<i&&num[n%3])
-			count++;
+		count+=num[n%3]<i&&num[n%3];
 		num[n++%3]=i;
 	}
 	printf("%d",count);
