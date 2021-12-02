0f2e8928dc1e6f6480031985f20b2993d5c04c74
Jadeiin <92222981+Jadeiin@users.noreply.github.com>
2021-12-02 23:24:53 +0800

    Create 2-1.c

---
+++ 2-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+	int i,x=0,y=0;
+	char cmd[8];
+	while(scanf("%s %d",cmd,&i)!=EOF)
+	{
+		if(cmd[0]=='f') x+=i;
+		else if(cmd[0]=='d') y+=i;
+		else if(cmd[0]=='u') y-=i;
+	}
+	printf("%d",x*y);
+	return 0;
+}
