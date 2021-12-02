96147509f7519e42d8d61498eca7b240cb62fa33
Jadeiin <92222981+Jadeiin@users.noreply.github.com>
2021-12-02 23:25:27 +0800

    Create 2-2.c

---
+++ 2-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+	int i,aim=0,x=0,y=0;
+	char cmd[8];
+	while(scanf("%s %d",cmd,&i)!=EOF)
+	{
+		if(cmd[0]=='f') x+=i,y+=aim*i;
+		else if(cmd[0]=='d') aim+=i;
+		else if(cmd[0]=='u') aim-=i;
+	}
+	printf("%d",x*y);
+	return 0;
+}
