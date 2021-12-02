665cc01ac84af81dedbfed58ead7f2905fffbf55
Jadeiin <92222981+Jadeiin@users.noreply.github.com>
2021-12-02 23:23:53 +0800

    Update 1-2.c
    Upgrade

--- 1-2.c
+++ 1-2.c
@@ -1,28 +1,12 @@
 #include <stdio.h>
 int main(void)
 {
-	int i,n=0,a=0,b=0,c=0,count=0;
+	int i,n=0,num[3]={0},count=0;
 	while(scanf("%d",&i)!=EOF)
 	{
-		if(!(n%3))
-		{
-			if(a&&a<i)
-				count++;
-			a=i;	
-		}
-		else if(!((n+2)%3))
-		{
-			if(b&&b<i)
-				count++;
-			b=i;
-		}
-		else if(!((n+1)%3))
-		{
-			if(c&&c<i)
-				count++;
-			c=i;
-		}
-		n++;
+		if(num[n%3]<i&&num[n%3])
+			count++;
+		num[n++%3]=i;
 	}
 	printf("%d",count);
 	return 0;
